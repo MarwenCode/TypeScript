595105d522788552031411a2a11dcb1af371ea98
MarwenCode <marwen_jdidi@yahoo.com>
2023-08-03 00:38:38 +0200

    Type Assertions

--- project2/build/js/main.js
+++ project2/build/js/main.js
@@ -103,3 +103,30 @@ const addAll = (a, b, c) => {
         return a + b;
     }
 };
+//Rest Parameters 
+const total = (a, ...nums) => {
+    return a + nums.reduce((prev, curr) => prev + curr);
+};
+logMsg(total(1, 2));
+// convert to more or less specific 
+let aa = 'hello';
+let bb = a; // less specific 
+let cc = aa; // more specific 
+let d = 'world';
+let e = 'world';
+const addOrConcat = (a, b, c) => {
+    if (c === 'add')
+        return a + b;
+    return '' + a + b;
+};
+let myVal = addOrConcat(2, 2, 'concat');
+// Be careful! TS sees no problem - but a string is returned
+let nextVal = addOrConcat(2, 2, 'concat');
+//10 as string
+10;
+// The DOM 
+const img = document.querySelector('img');
+const myImg = document.getElementById('#img');
+const nextImg = document.getElementById('#img');
+// img.src
+// myImg.src
