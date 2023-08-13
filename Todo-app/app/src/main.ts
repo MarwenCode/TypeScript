ca9ea114ada37adb5270a766a3754db0fbbfd82c
MarwenCode <marwen_jdidi@yahoo.com>
2023-08-13 23:35:30 +0200

    start react app

--- Todo-app/app/src/main.ts
+++ Todo-app/app/src/main.ts
@@ -1,55 +1,57 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts';
+import { v4 as uuidV4 } from "uuid"
 
-import {v4 as uuidV4 } from "uuid"
-
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
-
-
-//////////
-const list = document.querySelector<HTMLUListElement>("#list");
-const form = document.getElementById("#new-task-form") as HTMLFormElement | null;
-const input = document.querySelector<HTMLInputElement>("#new-task-title");
-
-interface Task {
-  id: string,
-  title: string,
-  completed: boolean,
-  createdAt: Date,
+type Task = {
+  id: string
+  title: string
+  completed: boolean
+  createdAt: Date
 }
 
+const list = document.querySelector<HTMLUListElement>("#list")
+const form = document.getElementById("new-task-form") as HTMLFormElement | null
+const input = document.querySelector<HTMLInputElement>("#new-task-title")
+const tasks: Task[] = []
+// tasks.forEach(addListItem)
+
 form?.addEventListener("submit", e => {
-  e.preventDefault();
+  e.preventDefault()
 
-  const task: Task = {
+  if (input?.value == "" || input?.value == null) return
+
+  const newTask: Task = {
     id: uuidV4(),
-    title: input?.value || "",
+    title: input.value,
     completed: false,
-    createdAt: new Date()
+    createdAt: new Date(),
   }
+  tasks.push(newTask)
+  // saveTasks()
 
-
+  addListItem(newTask)
+  input.value = ""
 })
 
+function addListItem(task: Task) {
+  const item = document.createElement("li")
+  const label = document.createElement("label")
+  const checkbox = document.createElement("input")
+  checkbox.addEventListener("change", () => {
+    task.completed = checkbox.checked
+    // saveTasks()
+  })
+  checkbox.type = "checkbox"
+  checkbox.checked = task.completed
+  label.append(checkbox, task.title)
+  item.append(label)
+  list?.append(item)
+}
 
+// function saveTasks() {
+//   localStorage.setItem("TASKS", JSON.stringify(tasks))
+// }
 
+// function loadTasks(): Task[] {
+//   const taskJSON = localStorage.getItem("TASKS")
+//   if (taskJSON == null) return []
+//   return JSON.parse(taskJSON)
+// }
